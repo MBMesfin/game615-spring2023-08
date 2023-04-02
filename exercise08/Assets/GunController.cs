14cefe434214c8818afc3885691e04853ded8133
YihuaPu <44237498+YihuaPu@users.noreply.github.com>
2023-04-02 19:21:38 -0400

    added new particle effects
    made everything better

--- exercise08/Assets/GunController.cs
+++ exercise08/Assets/GunController.cs
@@ -7,7 +7,8 @@ public class GunController : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip shootSound;
     public float bulletLifetime = 3f;
-
+    public float spinSpeed = 500f;
+    public float bulletSpeed = 2000f;
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -20,7 +21,10 @@ public class GunController : MonoBehaviour
     void Shoot()
     {
         GameObject bullet = Instantiate(watermelonBullet, bulletSpawn.position, bulletSpawn.rotation);
-        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * 2000f);
+        ParticleSystem trail = bullet.GetComponentInChildren<ParticleSystem>();
+        trail.Play();
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * bulletSpeed);
+        bullet.GetComponent<Rigidbody>().AddTorque(bullet.transform.up * spinSpeed);
         Destroy(bullet, bulletLifetime);
     }
     void PlayShootSound()
