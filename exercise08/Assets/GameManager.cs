6a8f38ab4381bfd3b15d29cdde74bda6ccc19160
MBMesfin <120344206+MBMesfin@users.noreply.github.com>
2023-04-04 23:10:48 -0400

    add gun and some select code

--- exercise08/Assets/GameManager.cs
+++ exercise08/Assets/GameManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GunController selectedUnit;
+    public Animator uiPanel;
+    public TMP_Text theName;
     void Start()
     {
         
@@ -13,6 +18,20 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
         
+                if (selectedUnit != null)
+                {
+                    selectedUnit.selected = false;
+                   // selectedUnit.bodyRend.material.color = selectedUnit.defaultColor;
+
+                    selectedUnit = null;
+
+                    uiPanel.SetTrigger("uiPanelOff");
+                }
+            
+        }
+
     }
 }
--- exercise08/Assets/GunController.cs
+++ exercise08/Assets/GunController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GunController : MonoBehaviour
@@ -9,6 +10,20 @@ public class GunController : MonoBehaviour
     public float bulletLifetime = 3f;
     public float spinSpeed = 500f;
     public float bulletSpeed = 2000f;
+    public bool selected = false; 
+
+    public GameObject pineappleBullet;
+
+    public string gunName;
+
+    GameManager gm;
+
+    void Start()
+    {
+        GameObject gmObj = GameObject.Find("GameManagerObject");
+        gm = gmObj.GetComponent<GameManager>();
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -20,15 +35,62 @@ public class GunController : MonoBehaviour
 
     void Shoot()
     {
-        GameObject bullet = Instantiate(watermelonBullet, bulletSpawn.position + new Vector3(0, 0.1f, 0), bulletSpawn.rotation);
-        ParticleSystem trail = bullet.GetComponentInChildren<ParticleSystem>();
-        trail.Play();
-        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * bulletSpeed);
-        bullet.GetComponent<Rigidbody>().AddTorque(bullet.transform.up * spinSpeed);
-        Destroy(bullet, bulletLifetime);
+
+
+        //if (gm.selectedUnit != null)
+        //{
+            GameObject bullet = Instantiate(watermelonBullet, bulletSpawn.position + new Vector3(0, 0.1f, 0), bulletSpawn.rotation);
+            ParticleSystem trail = bullet.GetComponentInChildren<ParticleSystem>();
+            trail.Play();
+            bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * bulletSpeed);
+            bullet.GetComponent<Rigidbody>().AddTorque(bullet.transform.up * spinSpeed);
+            Destroy(bullet, bulletLifetime);
+
+            gm.uiPanel.SetTrigger("uiPanelOn");
+        //}
+
+
+        //GameObject bullet1 = Instantiate(pineappleBullet, bulletSpawn.position + new Vector3(0, 0.1f, 0), bulletSpawn.rotation);
+        //ParticleSystem trail1 = bullet1.GetComponentInChildren<ParticleSystem>();
+        //trail1.Play();
+        //bullet1.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * bulletSpeed);
+        //bullet1.GetComponent<Rigidbody>().AddTorque(bullet1.transform.up * spinSpeed);
+        //Destroy(bullet1, bulletLifetime);
+
+        //GameObject bullet2 = Instantiate(appleBullet, bulletSpawn.position + new Vector3(0, 0.1f, 0), bulletSpawn.rotation);
+        //ParticleSystem trail2 = bullet2.GetComponentInChildren<ParticleSystem>();
+        //trail2.Play();
+        //bullet2.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * bulletSpeed);
+        //bullet2.GetComponent<Rigidbody>().AddTorque(bullet2.transform.up * spinSpeed);
+        //Destroy(bullet2, bulletLifetime);
     }
     void PlayShootSound()
     {
         audioSource.PlayOneShot(shootSound);
+    }
+
+    private void OnMouseDown()
+    {
+
+        if (gm.selectedUnit != null)
+        {
+            // if we're here, something was already selected!
+            // 1. Deselect it
+            gm.selectedUnit.selected = false;
+            //gm.selectedUnit.bodyRend.material.color = gm.selectedUnit.defaultColor;
+        }
+        // 2. Select me!
+        selected = true;
+       // bodyRend.material.color = selectedColor;
+
+        if (gm.selectedUnit == null)
+        {
+            gm.uiPanel.SetTrigger("uiPanelOn");
+        }
+
+        gm.selectedUnit = this;
+
+
+       // gm.theName.text = gunName;
     }
 }
